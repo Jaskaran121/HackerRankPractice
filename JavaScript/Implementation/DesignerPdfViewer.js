c0d892195957088c653f23c17f911b4397144a0e
Jaskaran121 <jaskaran310193@gmail.com>
2020-09-21 22:41:34 -0400

    pdf viewer js

---
+++ JavaScript/Implementation/DesignerPdfViewer.js
@@ -0,0 +1,11 @@
+const designerPdfViewer = (h,word) => {
+    let maxH = 0;
+    for(let index = 0;index<h.length;index++){
+        const asciiCode = word.charCodeAt(index);
+        if(h[asciiCode-97] > maxH)
+            maxH = h[asciiCode-97];
+    }
+    return maxH * word.length;
+}
+
+// https://www.hackerrank.com/challenges/designer-pdf-viewer
